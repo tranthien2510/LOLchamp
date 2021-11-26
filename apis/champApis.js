a7c6c0af9ed5507b85c9eac91cb39ea12d0fa31f
Minhh <tranhanminh200@gmail.com>
2021-11-26 17:13:47 +0700

    none

--- apis/champApis.js
+++ apis/champApis.js
@@ -1,4 +1,4 @@
-import AxiosService from "../common/AxiosService";
+import AxiosService from "../common/AxiosService.js";
 import { URL_GET_ALL_CHAMP, URL_GET_CHAMP } from "../constants";
 
 export const getAllChampRequest = () => AxiosService.get(URL_GET_ALL_CHAMP);
